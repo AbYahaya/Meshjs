5060545f94d9e04aa4cb1ca921d434313b38c81e
AbYahaya <yabdulrauf911@gmail.com>
2025-06-14 12:45:27 +0100

    Prems

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,21 @@
 import MyWalletComponent from "@/components/MyWalletComponent";
 import MyWalletAssetComponent from "@/components/MyWalletAssetComponent";
 import { CardanoWallet } from "@meshsdk/react";
+import Navbar from "@/components/NavBar";
 
 
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <h1>Welcome to the Mesh PBL Module 101</h1>
-        <CardanoWallet />
-        <MyWalletComponent />
-      </div>
-      <h2>Assets</h2>
-      <p>Here are the assets in your wallet:</p>
-      <div>
-        <h2>My Wallet Assets</h2>
-        <p>Here are the assets in your wallet:</p>
-        <MyWalletAssetComponent />
-      </div>
-    </>
+    <div>
+      <h1>Welcome to the Mesh PBL Module 101</h1>
+      <CardanoWallet />
+      
+  <Navbar/>
+</div>
+      
+    
+
+    
   );
 }
